f2831b8ae7486b2045895fb71a45c556551143cb
vi-SSH-al <vishalmourya229@gmail.com>
2024-08-19 11:16:36 +0530

    Update Navbar.tsx

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -14,10 +14,10 @@ const Navbar = () => {
           src="assets/images/site-logo.svg"
           width={23}
           height={23}
-          alt="DevFlow"
+          alt="DevCrux"
         />
         <p className="h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden">
-          Dev <span className="text-primary-500"> Overflow</span>
+          Dev <span className="text-primary-500"> Crux</span>
         </p>
       </Link>
       <GlobalSearch />

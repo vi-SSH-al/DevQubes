3a3a6134252220f2a318be4d2f97fdf01f804e78
vi-SSH-al <vishalmourya229.com>
2024-08-19 11:14:25 +0530

    Uploading Source code

---
+++ app/layout.tsx
@@ -0,0 +1,55 @@
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
+import "./globals.css";
+import "../styles/prism.css";
+import { ThemeProvider } from "@/context/ThemeProvider";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--foot-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+export const metadata: Metadata = {
+  title: "Devflow",
+  description:
+    " A commuinty-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topic in web development, mobile app development, algorithms, data structures, and more .",
+  icons: {
+    icon: "../public/assets/images/site-logo.svg",
+  },
+};
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+        <ClerkProvider
+          appearance={{
+            elements: {
+              formButtonPrimary: "primary-gradient",
+              footerActionLink: "primary-text-gradient hover:text-primary-500",
+            },
+          }}
+          afterSignOutUrl="/"
+        >
+          <ThemeProvider>{children}</ThemeProvider>
+        </ClerkProvider>
+      </body>
+    </html>
+  );
+}

ac3485ca530fb1397ac60e601408f9a9053dd6a5
vi-SSH-al <vishalmourya229.com>
2024-08-25 16:00:56 +0530

    updating mongoose.ts

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -13,7 +13,7 @@ export const connectToDatabase = async () => {
 
   try {
     await mongoose.connect(process.env.MONGODB_URL, {
-      dbName: "devQubesDB",
+      dbName: "devqubesdb",
     });
     isConnected = true;
     console.log("MongoDB is connected");

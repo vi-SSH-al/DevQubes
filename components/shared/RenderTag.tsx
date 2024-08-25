23ef60f6a2041b8046d9e66c68ef86ffc4b3485c
vi-SSH-al <vishalmourya229.com>
2024-08-25 19:02:16 +0530

    fixed : No. of questions in a Tag is not visible in the Right Sidebar

--- components/shared/RenderTag.tsx
+++ components/shared/RenderTag.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import React from "react";
-import { Badge } from "@/components/ui/badge";
+import { Badge } from "../ui/badge";
 
 interface Props {
   _id: string;
@@ -9,18 +9,20 @@ interface Props {
   showCount?: boolean;
 }
 
-const RenderTag = ({ _id, name, totalQuestions, showCount }: Props) => {
+export default function RenderTag({
+  _id,
+  totalQuestions,
+  name,
+  showCount,
+}: Props) {
   return (
     <Link href={`/tags/${_id}`} className="flex justify-between gap-2">
       <Badge className="subtle-medium background-light800_dark300 text-light400_light500 rounded-md border-none px-4 py-2 uppercase">
         {name}
       </Badge>
-
       {showCount && (
-        <p className="small-medium text-dark500_light700">{totalQuestions}</p>
+        <p className="small-medium  text-dark500_light700">{totalQuestions}</p>
       )}
     </Link>
   );
-};
-
-export default RenderTag;
+}
